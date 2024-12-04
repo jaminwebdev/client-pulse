c0408fa6abaaeeb482b7b2d6177755e42435b5ea
Jamin <jaminwebdev@gmail.com>
2024-12-04 11:50:24 -0600

    homepage and form shell

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,6 @@
+import { PatientForm } from "@/components/forms/PatientForm";
 import Image from "next/image";
+import Link from "next/link";
 
 
 export default function Home() {
@@ -13,8 +15,24 @@ export default function Home() {
               alt="patient"
               className="mb-12 h-10 w-fit"
             />
+            <PatientForm />
+            <div className="text-14-regular mt-20 flex justify-between">
+              <p className="justify-items-end text-dark-600 xl:text-left">
+                © 2024 ClientPulse
+              </p>
+              <Link href="/?admin=true" className="text-sky-500">
+                Admin
+              </Link>
+          </div>
         </div>
       </section>
+      <Image
+        src="/assets/images/onboarding.png"
+        height={1000}
+        width={1000}
+        alt="patient"
+        className="side-img max-w-[50%]"
+      />
     </div>
   );
 }
---
+++ components/forms/PatientForm.tsx
@@ -0,0 +1,7 @@
+"use client";
+
+export const PatientForm = () => {
+  return (
+    <h1>Patient form</h1>
+  )
+} 
