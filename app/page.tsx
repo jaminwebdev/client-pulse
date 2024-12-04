5057082ec48ded4e4ef4a725da2410679d6587b7
Jamin <jaminwebdev@gmail.com>
2024-12-04 11:23:41 -0600

    logo added

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,20 @@
-import { Button } from "@/components/ui/button";
+import Image from "next/image";
 
 
 export default function Home() {
   return (
-    <div>
-      <h1>Hello world</h1>
-      <Button>
-        Howdy
-      </Button>
+    <div className="flex h-screen max-h-screen">
+      <section className="remove-scrollbar container my-auto">
+        <div className="sub-container max-w-[496px]">
+          <Image
+              src="/assets/icons/Logo.svg"
+              height={1000}
+              width={1000}
+              alt="patient"
+              className="mb-12 h-10 w-fit"
+            />
+        </div>
+      </section>
     </div>
   );
 }
