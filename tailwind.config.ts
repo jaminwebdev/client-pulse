3bb987e4fcf0cb5af3bf91576e0e0a095dfc54ff
Jamin <jaminwebdev@gmail.com>
2024-12-09 12:55:10 -0600

    remove unnecessary bg gradients

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -49,11 +49,6 @@ const config = {
       fontFamily: {
         sans: ["var(--font-sans)", ...fontFamily.sans],
       },
-      backgroundImage: {
-        appointments: "url('/assets/images/appointments-bg.png')",
-        pending: "url('/assets/images/pending-bg.png')",
-        cancelled: "url('/assets/images/cancelled-bg.png')",
-      },
       keyframes: {
         "accordion-down": {
           from: { height: "0" },
