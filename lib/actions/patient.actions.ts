1798d43b5f3b6290ffe7dea3d76c6fd450a16798
Jamin <jaminwebdev@gmail.com>
2024-12-16 14:34:35 -0600

    add sentry

--- lib/actions/patient.actions.ts
+++ lib/actions/patient.actions.ts
@@ -14,10 +14,9 @@ import {
 } from "../appwrite.config";
 import { parseStringify } from "../utils";
 
-// CREATE APPWRITE USER
 export const createUser = async (user: CreateUserParams) => {
   try {
-    // Create new user -> https://appwrite.io/docs/references/1.5.x/server-nodejs/users#create
+    // https://appwrite.io/docs/references/1.5.x/server-nodejs/users#create
     const newuser = await users.create(
       ID.unique(),
       user.email,
@@ -27,7 +26,6 @@ export const createUser = async (user: CreateUserParams) => {
     );
     return parseStringify(newuser);
   } catch (error: unknown) {
-    // Check existing user
     if (error && (error as AppwriteException)?.code === 409) {
       const existingUser = await users.list([
         Query.equal("email", [user.email]),
@@ -39,7 +37,6 @@ export const createUser = async (user: CreateUserParams) => {
   }
 };
 
-// GET USER
 export const getUser = async (userId: string) => {
   try {
     const user = await users.get(userId);
@@ -53,13 +50,12 @@ export const getUser = async (userId: string) => {
   }
 };
 
-// REGISTER PATIENT
 export const registerPatient = async ({
   identificationDocument,
   ...patient
 }: RegisterUserParams) => {
   try {
-    // Upload file ->  // https://appwrite.io/docs/references/cloud/client-web/storage#createFile
+    // https://appwrite.io/docs/references/cloud/client-web/storage#createFile
     let file;
     if (identificationDocument) {
       const inputFile =
@@ -72,7 +68,7 @@ export const registerPatient = async ({
       file = await storage.createFile(BUCKET_ID!, ID.unique(), inputFile);
     }
 
-    // Create new patient document -> https://appwrite.io/docs/references/cloud/server-nodejs/databases#createDocument
+    // https://appwrite.io/docs/references/cloud/server-nodejs/databases#createDocument
     const newPatient = await databases.createDocument(
       APPWRITE_DB_ID!,
       PATIENT_COLLECTION_ID!,
@@ -92,7 +88,6 @@ export const registerPatient = async ({
   }
 };
 
-// GET PATIENT
 export const getPatient = async (userId: string) => {
   try {
     const patients = await databases.listDocuments(
